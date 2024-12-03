8da287ff3eab7cb0937de93a26fd2ffb774e85b3
Sharanya Jegadeesan <sharanyajegadeesan1999@gmail.com>
2024-12-03 14:44:02 +0530

    create database collection and connect

--- backend/db/connectDB.js
+++ backend/db/connectDB.js
@@ -3,7 +3,10 @@ import mongoose from "mongoose";
 export const connectDB = async () =>{
     try {
         console.log("mongo_uri: ", process.env.MONGO_URI);
-        const con = await mongoose.connect(process.env.MONGO_URI)
+        const con = await mongoose.connect(process.env.MONGO_URI,{
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
         console.log(`Mongoose Connected : ${con.connection.host}`);
         
     } catch (error) {
