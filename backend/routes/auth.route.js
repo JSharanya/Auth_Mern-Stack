95f2b014e6b0392fc30c2cb08fa31ad8dae18ea6
Sharanya Jegadeesan <sharanyajegadeesan1999@gmail.com>
2024-12-05 18:04:47 +0530

    set verify token

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,13 +1,24 @@
-import express from 'express'
-import { login, logout, signup, verifyEmail, forgotPassword, resetPassword} from '../controllers/auth.controller.js';
+import express from "express";
+import {
+  login,
+  logout,
+  signup,
+  verifyEmail,
+  forgotPassword,
+  resetPassword,
+ 
+} from "../controllers/auth.controller.js";
+import { verifyToken } from "../middleware/verifyToken.js";
+
 
 const router = express.Router();
 
-router.post("/signup", signup)
-router.post("/login", login)
-router.post("/logout",logout)
+router.get("/check-auth", verifyToken);
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout", logout);
 
-router.post("/verify-email", verifyEmail)
+router.post("/verify-email", verifyEmail);
 
 router.post("/forgot-password", forgotPassword);
 
